120e810d884800d87293f3d6ed4baed9e245e523
Sergey Klunniy <s.klunniy@gmail.com>
2023-06-10 18:41:27 +0300

    commit hello-world

--- src/main/java/ua/klunniy/springcourse/HelloController.java
+++ src/main/java/ua/klunniy/springcourse/HelloController.java
@@ -2,6 +2,8 @@ package ua.klunniy.springcourse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author Serhii Klunniy
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
+    //    @RequestMapping({"/hello"})
+//    @ResponseBody
     @GetMapping("/hello-world")
     public String sayHello() {
         return "hello_world";
