d037c0844056ff2ec1fd5bee6f7ea23979195830
Sergey Klunniy <s.klunniy@gmail.com>
2023-06-11 20:22:58 +0300

    commit name and surname

--- src/main/java/ua/klunniy/springcourse/controllers/FirstController.java
+++ src/main/java/ua/klunniy/springcourse/controllers/FirstController.java
@@ -3,6 +3,7 @@ package ua.klunniy.springcourse.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -17,6 +18,13 @@ public class FirstController {
     public String helloPage(HttpServletRequest request) {
         String name = request.getParameter("name");
         String surname = request.getParameter("surname");
+        System.out.println("Hello " + name + " " + surname);
+        return "first/hello";
+    }
+
+    @GetMapping("/hello-world")
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
         System.out.println("Hello " + name + " " + surname);
         return "first/hello";
     }
