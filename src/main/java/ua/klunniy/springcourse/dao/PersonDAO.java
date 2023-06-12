5f147d850ad1bf3d1ebccf52543f47fbf739f78e
Sergey Klunniy <s.klunniy@gmail.com>
2023-06-12 23:38:07 +0300

    add lambda get people by id

---
+++ src/main/java/ua/klunniy/springcourse/dao/PersonDAO.java
@@ -0,0 +1,35 @@
+package ua.klunniy.springcourse.dao;
+
+import ua.klunniy.springcourse.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Serhii Klunniy
+ */
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+        people.add(new Person(++PEOPLE_COUNT, "Kate"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public static void main(String[] args) {
+        PersonDAO personDAO = new PersonDAO();
+        System.out.println(personDAO.show(2));
+    }
+}
