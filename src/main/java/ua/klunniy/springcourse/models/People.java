0b8dba4d9097706e5e6e2e382a2251d88635043e
Sergey Klunniy <s.klunniy@gmail.com>
2023-06-12 23:07:38 +0300

    add controller People

--- src/main/java/ua/klunniy/springcourse/models/People.java
+++ src/main/java/ua/klunniy/springcourse/models/People.java
@@ -1,7 +1,12 @@
 package ua.klunniy.springcourse.models;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @author Serhii Klunniy
  */
+@Controller
+@RequestMapping("/people")
 public class People {
 }
