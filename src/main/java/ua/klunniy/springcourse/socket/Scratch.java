51251c650e5150304b2dcc00953cc5e3494b09aa
Serhii Klunniy <s.klunniy@gmail.com>
2023-06-20 17:08:18 +0300

    add Scratch

--- src/main/java/ua/klunniy/springcourse/socket/Scratch.java
+++ src/main/java/ua/klunniy/springcourse/socket/Scratch.java
@@ -1,7 +1,6 @@
 package ua.klunniy.springcourse.socket;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -11,11 +10,25 @@ public class Scratch {
 
         try (Socket socket = new Socket("localhost", 8085)) {
             PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+             new Thread(() -> {
+                 while (true) {
+                     printWriter.println("hello my socket");
+                     printWriter.println(scanner.nextLine());
+                 }
+            }).start();
 
-            while (true) {
-                printWriter.println("hello my socket");
-                printWriter.println(scanner.nextLine());
+
+            InputStream inputStream = socket.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String word;
+
+            while ((word = bufferedReader.readLine()) != null) {
+                System.out.println(word);
             }
+
+
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
