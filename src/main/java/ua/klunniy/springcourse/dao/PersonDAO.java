d705e68f1ea5e4d8521ad2ab089401dde6004de2
Sergey Klunniy <s.klunniy@gmail.com>
2023-08-06 21:16:44 +0300

    commit

--- src/main/java/ua/klunniy/springcourse/dao/PersonDAO.java
+++ src/main/java/ua/klunniy/springcourse/dao/PersonDAO.java
@@ -1,8 +1,10 @@
 package ua.klunniy.springcourse.dao;
 
 import org.springframework.stereotype.Component;
+import ua.klunniy.springcourse.enums.Gender;
 import ua.klunniy.springcourse.models.Person;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,25 +13,74 @@ import java.util.List;
  */
 @Component
 public class PersonDAO {
-    private static int PEOPLE_COUNT;
-    private static List<Person> people;
 
-    {
-        people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT, 24, "Tom"));
-        people.add(new Person(++PEOPLE_COUNT, 34, "Bob"));
-        people.add(new Person(++PEOPLE_COUNT, 44, "Mike"));
-        people.add(new Person(++PEOPLE_COUNT, 54, "Kate"));
+    private static final String URL = "jdbc:postgresql://localhost:5432/shop";
+    private static final String LOGIN = "postgres";
+    private static final String PASSWORD = "test";
+
+    private static final Connection connection;
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+
+    //      private static int PEOPLE_COUNT;
+    //      private static final Logger logger = Logger.getLogger(DbConnector.class);
+    //      private static List<Person> people;
+
+//    {
+//        people = new ArrayList<>();
+//        people.add(new Person(++PEOPLE_COUNT, 24, "Tom"));
+//        people.add(new Person(++PEOPLE_COUNT, 34, "Bob"));
+//        people.add(new Person(++PEOPLE_COUNT, 44, "Mike"));
+//        people.add(new Person(++PEOPLE_COUNT, 54, "Kate"));
+//    }
 
     //в этом методе мы просто возвращаем список из людей
     public List<Person> index() {
+
+        List<Person> people = new ArrayList<>();
+        String sql = "SELECT * FROM Person";
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+
+            while (rs.next()) {
+                long id = rs.getLong("id");
+                int age = rs.getInt("age");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                Gender gender = Gender.valueOf(rs.getString("gender"));
+
+                Person person = new Person(id, age, name, email, gender);
+                people.add(person);
+            }
+
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         return people;
     }
 
     //второй метод возвращает одного человека
     public Person show(int id) {
-        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+        //return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+        return null;
     }
 
     public static void main(String[] args) {
@@ -38,21 +89,21 @@ public class PersonDAO {
     }
 
     public static void save(Person person) {
-        if (person != null) {
-            person.setId(++PEOPLE_COUNT);
-            people.add(person);
-        }
+//        if (person != null) {
+//            person.setId(++PEOPLE_COUNT);
+//            people.add(person);
+//        }
     }
 
     public void update(int id, Person updatePerson) {
-        Person personToBeUpdated = show(id);
-        personToBeUpdated.setName(updatePerson.getName());
-        personToBeUpdated.setSurname(updatePerson.getSurname());
-        personToBeUpdated.setEmail(updatePerson.getEmail());
-        personToBeUpdated.setAge(updatePerson.getAge());
+//        Person personToBeUpdated = show(id);
+//        personToBeUpdated.setName(updatePerson.getName());
+//        personToBeUpdated.setSurname(updatePerson.getSurname());
+//        personToBeUpdated.setEmail(updatePerson.getEmail());
+//        personToBeUpdated.setAge(updatePerson.getAge());
     }
 
     public void delete(int id) {
-        people.removeIf(p -> p.getId() == id);
+        //people.removeIf(p -> p.getId() == id);
     }
 }
--- src/main/java/ua/klunniy/springcourse/models/Person.java
+++ src/main/java/ua/klunniy/springcourse/models/Person.java
@@ -19,15 +19,13 @@ import ua.klunniy.springcourse.enums.Gender;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Person {
 
-    int id;
+    long id;
     @Min(value = 0, message = "age should be greater then 0")
     int age;
 
     @NotEmpty(message = "Name should not be empty") //не может быть нулл и не может быть пустое сообщение
     @Size(min = 2, max = 10, message = "Name should be 2 and 10 characters")
     String name;
-    String surname;
-    String lastName;
 
     @NotEmpty(message = "Email should not be empty") //не может быть нулл и не может быть пустое сообщение
     @Email(message = "Email should be valid")
